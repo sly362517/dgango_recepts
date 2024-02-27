361c5f4c544ac25f2dc0720ae7f3dcac7f8cc843
sly362517 <machortovdima@mail.ru>
2024-02-27 04:40:37 +0300

    added project!!

--- recipes/recipes/settings.py
+++ recipes/recipes/settings.py
@@ -54,6 +54,7 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
